cc91caf26c9a865cd498982007b1193fbde4493d
Elaine Tsun <elaine.tsun3@gmail.com>
2018-04-29 10:46:21 -0400

    comments

--- test.pl
+++ test.pl
@@ -8,10 +8,26 @@ squint([X|Xs],[X|Ys]) :- squint(Xs, Ys).
 % compress - collapses sequences of consecitive idential ground terms
 compress([],[]).
 compress([X],[X]).
-compress([X,X|T],NL):-compress([X|T],NL),!.
-compress([X,Y|T],[X|NT]):-compress([Y|T],NT).
+compress([X,X|T],NL):-
+  % if the head is the same as the next element
+  compress([X|T],NL),!.
+compress([X,Y|T],[X|NT]):-
+  % if the head is different than the next element
+  compress([Y|T],NT).
 
 % my_flatten - flattens into a one-level list
-my_flatten(X,[X]) :- \+ is_list(X).
+my_flatten(X,[X]) :- \+ is_list(X). % check if input is a list
 my_flatten([],[]).
-my_flatten([X|Xs],Zs) :- my_flatten(X,Y), my_flatten(Xs,Ys), append(Y,Ys,Zs).
+my_flatten([X|Xs],Zs) :-
+  % recursively flatten the list
+  my_flatten(X,Y),
+  my_flatten(Xs,Ys),
+  append(Y,Ys,Zs).
+
+% pack - produces a list containing the consecutive reps of a ground term
+pack([], []).
+pack([X], [[X]]).
+pack([X, Y|Ys], [[X]|Z]) :-
+    X \= Y, pack([Y|Ys], Z).
+pack([X, X|Xs], [[X|XX]|Y]):-
+   pack([X|Xs], [XX|Y]).
